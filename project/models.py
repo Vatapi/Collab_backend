9d9fff58dd44c0653820cdeeb013b2db2d7ac080
Deepak Hirawat <bit.deepak99@gmail.com>
2019-02-06 00:00:21 +0530

    minor changes

--- project/models.py
+++ project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    choices_project = (
+        ('completed' , 'completed'),
+        ('collab' , "looking for collaboration")
+    )
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    status = models.CharField(max_length = 10, choices = choices_project)
+    skills = models.TextField()
+    user_posted = models.ForeignKey(to=User , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- project/serializers.py
+++ project/serializers.py
@@ -5,15 +5,11 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
      class Meta:
          model = User
-         fields = ('username', 'email')
+         fields = ('username' , 'email')
 
 class UserSerializerWithToken(serializers.ModelSerializer):
     token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only = True , min_length = 8)
-    # first_name = serializers.CharField(max_length = 30)
-    # last_name = serializers.CharField(max_length = 30)
-    email = serializers.EmailField()
-    # username = serializers.CharField(max_length = 50)
 
     def get_token(self, obj):
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -33,4 +29,4 @@ class UserSerializerWithToken(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username' ,'email', 'password' , 'token',)
+        fields = ('username','email','password','token',)
